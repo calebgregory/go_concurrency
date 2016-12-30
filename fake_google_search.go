6b1ef4a9c27b654777b5655540332b75245fd25f
Caleb Gregory <calebgregory@Calebs-MBP.industrious>
2016-12-30 15:29:23 -0600

    Adding channels for results, enabling parallel processing (the composition of which is handled by Go's concurrency model)

--- fake_google_search.go
+++ fake_google_search.go
@@ -23,9 +23,22 @@ func fakeSearch(kind string) Search {
 }
 
 func Google(query string) (results []Result) {
-	results = append(results, Web(query))
-	results = append(results, Image(query))
-	results = append(results, Video(query))
+	c := make(chan Result)
+
+	go func() { c <- Web(query) }()
+	go func() { c <- Image(query) }()
+	go func() { c <- Video(query) }()
+
+	timeout := time.After(80 * time.Millisecond)
+	for i := 0; i < 3; i++ {
+		select {
+		case result := <-c:
+			results = append(results, result)
+		case <-timeout:
+			fmt.Println("timed out")
+			return
+		}
+	}
 	return
 }
 
